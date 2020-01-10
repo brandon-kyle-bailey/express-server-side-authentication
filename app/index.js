a84ec71428ab36e69846975d1a9bb37c57189964
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-01-10 22:03:06 +0000

    adding submit post request route

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
-let urlencoded = bodyParser.urlencoded({ urlencoded: false });
+let urlencoded = bodyParser.urlencoded({ urlencoded: false, extended: true });
 
 const __dirname = path.resolve();
 const port = process.env.PORT || 8080;
@@ -15,5 +15,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+});
 
 app.listen(port, () => console.log(`Server running, visit http://localhost/${port}`));
