c023718868b60a63f6d56b52e1f04a5dcb57c93c
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-01-10 21:26:55 +0000

    creation of public directory with js and css files. adding index.html as sendfile on get request of home page

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send("Success!");
+    // res.send("Success!");
+    res.sendFile(path.join(path.resolve() + '/index.html'));
 })
 
 app.listen(port, () => console.log(`Server running, visit http://localhost/${port}`));
