7b6bdf4d8334c1d540d58674d1cad3ee540cb7d7
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-01-10 21:59:52 +0000

    adding urlencoded

--- app/index.js
+++ app/index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
+let urlencoded = bodyParser.urlencoded({ urlencoded: false });
+
 const __dirname = path.resolve();
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(urlencoded);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
