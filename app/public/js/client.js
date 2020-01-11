0327fe4b5715ecd046332055b7d56b3f6f0ff6c9
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-01-11 00:14:49 +0000

    wrapping error list display in an if statement to avoid reference error of map on undefined errors array

--- app/public/js/client.js
+++ app/public/js/client.js
@@ -7,7 +7,11 @@ function submitData(e) {
         headers: {
             'Content-type': 'application/json'
         },
-        body: JSON.stringify({ name: formData.get('name'), email: formData.get('email') }),
+        body: JSON.stringify({
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password')
+        }),
         method: 'POST'
     };
 
@@ -15,12 +19,17 @@ function submitData(e) {
         .then(res => res.json())
         .then(data => {
 
-            document.querySelector('.error-container').style.display = 'block';
-            const error = document.querySelector('.error');
+            if (data.errors) {
+                document.querySelector('.error-container').style.display = 'block';
+                const error = document.querySelector('.error');
 
-            error.innerHTML = data.errors.map(error => {
-                return `<li>${error.msg}</li>`
-            }).join('');
+                error.innerHTML = data.errors.map(error => {
+                    return `<li>${error.msg}</li>`
+                }).join('');
+            } else {
+                document.querySelector('.error-container').style.display = 'none';
+                console.log({ "Success": data });
+            }
 
         })
         .catch(err => console.log(err))
